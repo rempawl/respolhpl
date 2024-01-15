ef18272cb2fa780f1727c699be53a4c2c050d649
rempawl <rem.pawl@gmail.com>
2024-01-15 10:35:27 +0100

    started working on checkout

---
+++ app/src/main/java/com/rempawl/respolhpl/data/usecase/ClearCartUseCase.kt
@@ -0,0 +1,10 @@
+package com.rempawl.respolhpl.data.usecase
+
+import com.rempawl.respolhpl.data.sources.repository.CartRepository
+import javax.inject.Inject
+
+class ClearCartUseCase @Inject constructor(private val repo: CartRepository) : ActionResultUseCase<Unit, Unit>() {
+    override suspend fun doWork(parameter: Unit) {
+        repo.clearCart()
+    }
+}
